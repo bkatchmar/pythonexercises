0455dbdcf35fbfb02297fbc3e245a4292d99f96b
Brian Katchmar <bkatchmar@gmail.com>
2020-01-30 17:09:17 -0500

    Change Print Message

--- make-api-call.py
+++ make-api-call.py
@@ -6,8 +6,8 @@ response = requests.get(api_endpoint).json()
 paging_info = response["info"]
 characters = response["results"]
 
-print(paging_info)
-print(characters)
+# print(paging_info)
+# print(characters)
 
 for ruler in characters:
-    print("%s" % (ruler["name"]))
+    print("%s - %s %s" % (ruler["name"], ruler["species"], ruler["gender"]))
